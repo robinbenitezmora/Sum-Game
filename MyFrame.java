757260b5e796e19d469511c25295a1fd1e7a2751
robinbenitezmora <robinbenitez1990@gmail.com>
2024-01-14 11:56:12 -0500

    Add more code in MyFrame class

--- MyFrame.java
+++ MyFrame.java
@@ -1,3 +1,16 @@
-public class MyFrame {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+public class MyFrame extends JFrame {
+ public MyFrame() {
+  super("My Frame");
+  setSize(400, 300);
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  JPanel panel = new JPanel();
+  JLabel label = new JLabel();
+  panel.add(label);
+  add(panel);
+  setVisible(true);
+ }
 }
